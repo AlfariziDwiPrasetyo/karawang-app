e0083ac8f737d85ed6f6e1b19178ff88b8f62a47
ashafaatadhis <skullreaper387@gmail.com>
2023-12-06 08:46:20 +0700

    chore(admin): layout admin page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,45 @@
+"use client";
 import { RxHamburgerMenu } from "react-icons/rx";
 import Container from "@/components/Container";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import SidebarMenu from "./SidebarMenu";
+
 export default function NavbarAdmin() {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <nav className="md:hidden block">
-      <Container className="flex items-center justify-between m-4 mx-auto">
-        <button className="border p-2 rounded">
-          <RxHamburgerMenu />
-        </button>
-        <span className="">ADMIN PAGE</span>
-      </Container>
-    </nav>
+    <>
+      <nav className="md:hidden block">
+        <Container className="flex items-center justify-between m-4 mx-auto">
+          <button
+            onClick={() => setIsActive((v) => !v)}
+            className="border p-2 rounded"
+          >
+            <RxHamburgerMenu />
+          </button>
+          <span className="">ADMIN PAGE</span>
+        </Container>
+      </nav>
+      <div
+        className={`fixed inset-4 rounded-md p-8 bg-slate-300 overflow-scroll  ${
+          isActive ? "block" : "hidden"
+        }`}
+      >
+        <div className="flex justify-between items-center">
+          <h1>ADMIN PAGE</h1>
+          <button
+            onClick={() => setIsActive((v) => !v)}
+            className="border p-2 rounded-md"
+          >
+            <IoMdClose />
+          </button>
+        </div>
+        <div className="mt-7">
+          <SidebarMenu className={"py-5"} />
+          <SidebarMenu className={"py-5"} />
+          <SidebarMenu className={"py-5"} />
+        </div>
+      </div>
+    </>
   );
 }
--- components/SidebarMenu.jsx
+++ components/SidebarMenu.jsx
@@ -1,6 +1,6 @@
-const SidebarMenu = () => {
+const SidebarMenu = ({ className }) => {
   return (
-    <div className="py-5 border-slate-800 border-b">
+    <div className={`border-slate-800 border-t ${className}`}>
       <h2>ADMIN LAYOUT PAGES</h2>
       <ul className="list-none flex-col flex mt-5 gap-3">
         <li>DASHBOARD</li>
