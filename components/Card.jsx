122fdd7aefb4d4f106b1fef6f11eaf6eec6a8d45
Ryuugamine <yoerdanatan@gmail.com>
2023-12-12 10:44:41 +0700

    pull backup branch

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Card = ({ image, title, description }) => {
+const Card = ({ image, title, content }) => {
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg m-4">
       <div className="aspect-w-16 aspect-h-9">
         <img className="object-cover w-full h-full" src={image} alt={title} />
       </div>
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{description}</p>
+        <a className="font-bold text-xl mb-2">{title}</a>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
       </div>
     </div>
   );
