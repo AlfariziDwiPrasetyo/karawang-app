5c38a251a1679b16df6eb008c8311933a6f358eb
Ryuugamine <yoerdanatan@gmail.com>
2023-12-14 01:00:48 +0700

    fix: error when parsing html to dom

--- components/ActivityPage.jsx
+++ components/ActivityPage.jsx
@@ -6,16 +6,16 @@ const DynamicPage = ({ title, content, image }) => {
       <div className="w-full mt-2 lg:mt-4 bg-yellow-600 text-white p-8 text-3xl">
         Kegiatan Keluarahan Nagasari
       </div>
+      <h1 className="text-center lg:px-5 mt-10 lg:text-left font-bold text-3xl lg:text-4xl">
+        {title}
+      </h1>
       <div className="mt-10 grid lg:grid-cols-2 px-5">
         <div className="w-full">
           <img src={image} className="w-full object-contain" alt="" />
         </div>
-        <div className=" lg:ml-5 p-3 lg:p-1">
-          <h1 className="text-center lg:px-5 lg:text-left font-bold text-3xl lg:text-4xl">
-            {title}
-          </h1>
+        <div className=" lg:ml-5 p-3 lg:px-1">
           <div
-            className="mt-5 text-justify"
+            className="text-justify"
             dangerouslySetInnerHTML={{ __html: content }}
           ></div>
         </div>
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Card = ({ image, title, content, id }) => {
+  const removedTagContent = (content) => {
+    let tmp = document.createElement("div");
+    tmp.innerHTML = content;
+    return tmp.textContent || tmp.innerText || "";
+  };
+  console.log(removedTagContent(content).length);
+
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg m-4">
       <div className="aspect-w-16 aspect-h-9 h-52">
@@ -10,10 +17,13 @@ const Card = ({ image, title, content, id }) => {
         <a href={`/artikel/${id}`} className="font-bold text-xl mb-2">
           {title}
         </a>
-        <div
-          className="mt-5 text-justify"
-          dangerouslySetInnerHTML={{ __html: content }}
-        ></div>
+        <div className="mt-5 text-justify">
+          {removedTagContent(content).length > 100 ? (
+            <p>{removedTagContent(content).slice(0, 100 - 6) + " ..."}</p>
+          ) : (
+            <p>{removedTagContent(content)}</p>
+          )}
+        </div>
       </div>
     </div>
   );
