8811481149c45393b44f9aaae3016eb0ac95b0e7
Ryuugamine <yoerdanatan@gmail.com>
2023-12-14 00:05:28 +0700

    dinamic content

--- components/NavSearch.jsx
+++ components/NavSearch.jsx
@@ -1,6 +1,26 @@
+"use client";
 import { MdEmail } from "react-icons/md";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const NavSearch = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    setSearchValue(e.target.value);
+    console.log(searchValue);
+    router.push("/search");
+  };
+
+  const keyPressHandle = (e) => {
+    if (e.key === "Enter") {
+      handleOnSubmit(e);
+    }
+  };
+
   return (
     <nav className="bg-red-800 z-50 fixed w-full top-0 text-white">
       <div className="container mx-auto flex justify-between items-center py-5 md:py-1 px-6 md:px-0">
@@ -15,6 +35,9 @@ const NavSearch = () => {
             type="text"
             placeholder="Search..."
             className="bg-red-700 placeholder-white text-white px-3 py-1 rounded-md focus:outline-none"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            onKeyDown={keyPressHandle}
           />
         </div>
       </div>
