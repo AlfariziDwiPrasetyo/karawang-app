1b5fd1767e82438e61951a9bc611acec95fdbb56
Ryuugamine <yoerdanatan@gmail.com>
2023-12-15 12:24:07 +0700

    add more content in umkm

--- app/(main)/umkm/page.jsx
+++ app/(main)/umkm/page.jsx
@@ -100,18 +100,111 @@ const page = () => {
                 </tr>
               </tbody>
             </table>
+          </div>
 
-            {/* isi */}
-            <p className="text-justify pt-5">
-              Sektor perdagangan dan jasa di Kelurahan Nagasari cukup dominan,
-              warga banyak yang mempunyai mata pencaharian dengan berdagang,
-              usaha berdagang berupa pertokoan yang besar sampai pedagang kaki
-              lima, dan ada juga usaha lainya berupa perbengkelan kendaraan
-              otomotif sampai pembuatan aksesoris serta usaha lainya karena
-              letak kelurahan Nagasari yang merupakan daerah perkotaan, sehingga
-              perekonomian dilingkungan Kelurahan Nagasari cukup menjanjikan
-              dimana usaha perdaganan dan jasa banyak yang berhasil.
-            </p>
+          {/* isi */}
+          <p className="text-justify pt-5">
+            Sektor perdagangan dan jasa di Kelurahan Nagasari cukup dominan,
+            warga banyak yang mempunyai mata pencaharian dengan berdagang, usaha
+            berdagang berupa pertokoan yang besar sampai pedagang kaki lima, dan
+            ada juga usaha lainya berupa perbengkelan kendaraan otomotif sampai
+            pembuatan aksesoris serta usaha lainya karena letak kelurahan
+            Nagasari yang merupakan daerah perkotaan, sehingga perekonomian
+            dilingkungan Kelurahan Nagasari cukup menjanjikan dimana usaha
+            perdaganan dan jasa banyak yang berhasil.
+          </p>
+          {/* tabel */}
+          <div className="overflow-x-auto mt-5">
+            <table className="min-w-full border-collapse border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="border border-gray-300 px-4 py-2">No</th>
+                  <th className="border border-gray-300 px-4 py-2">
+                    Jenis Usaha
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2">
+                    Banyaknya
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">1</td>
+                  <td className="border border-gray-300 px-4 py-2">Warung</td>
+                  <td className="border border-gray-300 px-4 py-2">375</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">2</td>
+                  <td className="border border-gray-300 px-4 py-2">Toko</td>
+                  <td className="border border-gray-300 px-4 py-2">241</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">3</td>
+                  <td className="border border-gray-300 px-4 py-2">Kios</td>
+                  <td className="border border-gray-300 px-4 py-2">71</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">4</td>
+                  <td className="border border-gray-300 px-4 py-2">Apotek</td>
+                  <td className="border border-gray-300 px-4 py-2">15</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">5</td>
+                  <td className="border border-gray-300 px-4 py-2">Bengkel</td>
+                  <td className="border border-gray-300 px-4 py-2">17</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">6</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    Furniture
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">6</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">7</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    Percetakan
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">7</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">8</td>
+                  <td className="border border-gray-300 px-4 py-2">Notaris</td>
+                  <td className="border border-gray-300 px-4 py-2">12</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">9</td>
+                  <td className="border border-gray-300 px-4 py-2">Wartel</td>
+                  <td className="border border-gray-300 px-4 py-2">2</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">10</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    Showroom / Dealer
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">3</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">11</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    Salon dan kecantikan
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">4</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">12</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    Jasa Pertukangan
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">153</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 px-4 py-2">13</td>
+                  <td className="border border-gray-300 px-4 py-2">Material</td>
+                  <td className="border border-gray-300 px-4 py-2">5</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
