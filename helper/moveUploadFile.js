0f735a8e4d40e86d0a1d6ac5b99ffb01a2c4630a
ashafaatadhis <skullreaper387@gmail.com>
2023-12-15 09:23:57 +0700

    percobaan

--- helper/moveUploadFile.js
+++ helper/moveUploadFile.js
@@ -1,25 +1,17 @@
-import streamifier from "streamifier";
 import v2 from "./cloudinary";
-import { Readable } from "stream";
 
 export default async function moveUploadFile(file, folder) {
-  return new Promise(async (res, rej) => {
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
+  const mime = file.type;
+  const encoding = "base64";
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes).toString("base64");
+  const fileUri = "data:" + mime + ";" + encoding + "," + buffer;
 
-    // const stream = createReadStream(buffer);
-    console.log(buffer);
-    const theTransformStream = v2.uploader.upload_stream(
-      {
-        folder: folder,
-        resource_type: "auto", // atau spesifikkan tipe sumber daya jika diperlukan (image, video, raw, dll.)
-      },
-      (err, result) => {
-        if (err) return rej(err);
-        res(result);
-      }
-    );
-
-    streamifier.createReadStream(buffer).pipe(theTransformStream);
+  const upload = await v2.uploader.upload(fileUri, {
+    invalidate: true,
+    folder: folder,
+    resource_type: "auto",
   });
+
+  return upload;
 }
