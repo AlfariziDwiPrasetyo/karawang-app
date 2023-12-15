37d7fbb315b112fdbf797879a0892b071b685b89
Ryuugamine <yoerdanatan@gmail.com>
2023-12-15 12:39:41 +0700

    feat: add 404 not found page

---
+++ app/(main)/not-found.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="mt-28 mb-52 items-center  justify-center">
+      <h1 className=" text-center font-bold text-5xl">Page Not Found</h1>
+      <h1 className="text-center mt-5 font-bold text-9xl">404</h1>
+    </div>
+  );
+};
+
+export default NotFound;
