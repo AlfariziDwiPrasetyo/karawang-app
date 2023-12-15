09431c56d3cebbb88ccc41ff57c93c11921a41e7
ashafaatadhis <skullreaper387@gmail.com>
2023-12-15 08:30:57 +0700

    fixing upload file joa

--- helper/moveUploadFile.js
+++ helper/moveUploadFile.js
@@ -2,16 +2,27 @@ import { writeFile, rm, mkdir } from "fs/promises";
 import { createReadStream } from "fs";
 import { dirname, join } from "path";
 import v2 from "./cloudinary";
+import { Readable } from "stream";
 
 export default async function moveUploadFile(file, folder) {
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
+  return new Promise(async (res, rej) => {
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-  const stream = createReadStream(buffer);
+    // const stream = createReadStream(buffer);
 
-  const uploaded = await v2.uploader.upload(stream, {
-    folder: folder,
-    resource_type: "auto", // atau spesifikkan tipe sumber daya jika diperlukan (image, video, raw, dll.)
+    const theTransformStream = v2.uploader.upload_stream(
+      {
+        folder: folder,
+        resource_type: "auto", // atau spesifikkan tipe sumber daya jika diperlukan (image, video, raw, dll.)
+      },
+      (err, result) => {
+        if (err) return rej(err);
+        res(result);
+      }
+    );
+
+    let str = Readable.from(buffer);
+    str.pipe(theTransformStream);
   });
-  return uploaded;
 }
