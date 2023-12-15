198863fc3a9f25ac98a152a5aaeb745af7d8c766
Ryuugamine <yoerdanatan@gmail.com>
2023-12-15 07:06:09 +0700

    fix layouting

--- components/LayananPage.jsx
+++ components/LayananPage.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const LayananPage = ({ name, content }) => {
   return (
-    <>
+    <div className="h-screen">
       <div className="w-full mt-1 lg:mt-4 bg-yellow-600 text-white p-8 text-3xl">
         {name}
       </div>
       <div className="mt-12 p-5">
         <div dangerouslySetInnerHTML={{ __html: content }}></div>
       </div>
-    </>
+    </div>
   );
 };
 
