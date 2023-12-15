e5e05e29aabd078e2a22c3f9440c419acb0ff440
ashafaatadhis <skullreaper387@gmail.com>
2023-12-15 08:03:25 +0700

    fixing upload file

--- helper/moveUploadFile.js
+++ helper/moveUploadFile.js
@@ -1,15 +1,17 @@
 import { writeFile, rm } from "fs/promises";
-import { join } from "path";
+import { dirname, join } from "path";
 import v2 from "./cloudinary";
 
-export default async function moveUploadFile(file, folder) {
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
+const bytes = await file.arrayBuffer();
+const buffer = Buffer.from(bytes);
 
-  const path = join("./temp", new Date().getTime() + "-" + file.name);
+const path = join("./temp", new Date().getTime() + "-" + file.name);
+const directory = dirname(path);
 
-  await writeFile(path, buffer);
-  const uploaded = await v2.uploader.upload(path, { folder: folder });
-  // await rm(path);
-  return uploaded;
-}
+// Membuat direktori jika belum ada
+await mkdir(directory, { recursive: true });
+
+await writeFile(path, buffer);
+const uploaded = await v2.uploader.upload(path, { folder: folder });
+// await rm(path);
+return uploaded;
