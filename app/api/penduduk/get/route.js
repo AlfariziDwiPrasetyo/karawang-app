6f2d348f86d80544fa4433c600804dad05ad2fc3
ashafaatadhis <skullreaper387@gmail.com>
2023-12-17 09:07:39 +0700

    fix: force dynamic 2

--- app/api/penduduk/get/route.js
+++ app/api/penduduk/get/route.js
@@ -1,6 +1,8 @@
 import prisma from "@/helper/prismaInit";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"; // defaults to force-static
+
 export async function GET(request) {
   try {
     const post = await prisma.penduduk.findFirstOrThrow({
