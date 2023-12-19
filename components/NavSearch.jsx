65f866b76b34befd236b707d59ad0234350b266f
ashafaatadhis <skullreaper387@gmail.com>
2023-12-19 10:23:03 +0700

    fix: search in mobile 2

--- components/NavSearch.jsx
+++ components/NavSearch.jsx
@@ -38,14 +38,14 @@ const NavSearch = () => {
 
   return (
     <nav className="bg-red-800 z-50 fixed w-full top-0 text-white">
-      <div className="container mx-auto flex justify-between items-center py-5 md:py-1 px-6 md:px-0">
+      <div className="container mx-auto flex justify-between items-center   py-1  px-0">
         <div className="flex items-center">
           {loading ? (
             <SpinnerLoad width={4} height={4} />
           ) : (
             <>
-              <MdEmail className="text-white mr-2 hidden md:flex" />
-              <a href="/" className="text-white hidden md:flex">
+              <MdEmail className="text-white mr-2 flex" />
+              <a href="/" className="text-white flex">
                 {data.data.email}
               </a>
             </>
